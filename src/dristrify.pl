dd074be064e570b2bdbb4f6f22698611c9fcf521
Digas29 <diogo@iMac-de-Diogo.home>
2015-10-11 16:40:35 +0100

    Inicio do Relatório

--- src/dristrify.pl
+++ src/dristrify.pl
@@ -1,4 +1,4 @@
-Board([
+emptyBoard([
 	[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
 	[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
 	[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
@@ -9,7 +9,7 @@ Board([
 	[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
 	[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell]]).
 
-IntermediumBoard([
+intermediumBoard([
 	[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, white, emptyCell],
 	[emptyCell, emptyCell, white, emptyCell, white, white, emptyCell, black, white],
 	[emptyCell, emptyCell, emptyCell, black, emptyCell, black, white, emptyCell, black],
@@ -20,21 +20,35 @@ IntermediumBoard([
 	[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, white, black, white, emptyCell],
 	[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell]]).
 
-FinalBoard([
+finalBoard([
 	[emptyCell, emptyCell, emptyCell, black, emptyCell, emptyCell, black, white, emptyCell],
 	[emptyCell, emptyCell, white, black, white, white, emptyCell, black, white],
 	[emptyCell, white, emptyCell, black, white, black, white, emptyCell, black],
 	[emptyCell, white, black, emptyCell, white, black, white, black, emptyCell],
 	[emptyCell, emptyCell, black, white, black, emptyCell, emptyCell, white, black],
-	[emptyCell, white, black, emptyCell, white, black, emptyCell, emptyCell, emptyCell],
-	[emptyCell, emptyCell, black, emptyCell, white, black, emptyCell, emptyCell, emptyCell],
+	[emptyCell, white, black, emptyCell, white, black, emptyCell, black, emptyCell],
+	[emptyCell, emptyCell, black, emptyCell, white, black, emptyCell, black, emptyCell],
 	[emptyCell, white, white, white, emptyCell, white, black, white, emptyCell],
 	[emptyCell, emptyCell, black, emptyCell, emptyCell, emptyCell, black, emptyCell, emptyCell]]).
 
+rowIdentifiers([' 1 ',' 2 ', ' 3 ',' 4 ',' 5 ',' 6 ',' 7 ',' 8 ', ' 9 ']).
 
-convertToChar(Cell, Char) :- (Cell =:= emptyCell -> Char = ' '; 
-	Cell =:= black -> Char = 'O'; 
-	Char  = 'X').
+getSymbol(emptyCell, ' ').
+getSymbol(white, 'O').
+getSymbol(black, '#').
 
-printBoardLine([Head|Tail]) :- (Head =/= 0 -> convertToChar(Head,Char), write(char), printBoardLine(Tail)).
-printBoard([Head|Tail]) :- (Head =/= 0 -> printBoardLine(Head), printBoard(Tail)).
+
+printColumnId :- write('      A     B     C     D     E     F     G     H     I'),nl.
+printInitialSeparator :- write('    _____________________________________________________ '),nl.
+printMiddleSeparator :- write('   |     |     |     |     |     |     |     |     |     |'),nl.
+printFinalSeparator :-  write('   |_____|_____|_____|_____|_____|_____|_____|_____|_____|'),nl.
+
+printCell(Char) :- write('|  '), write(Char), write('  ').
+printBoardLine([]) :- write('|'), nl, printFinalSeparator.
+printBoardLine([Head|Tail]) :- getSymbol(Head,Char), printCell(Char), printBoardLine(Tail).
+
+printBoardAux([],[]) :- nl.
+printBoardAux([Head|Tail], [RowId|RowTail]) :- printMiddleSeparator, write(RowId), printBoardLine(Head), printBoardAux(Tail, RowTail).
+
+printBoard(Board) :- printColumnId, printInitialSeparator, rowIdentifiers(RowId), printBoardAux(Board, RowId).
+
