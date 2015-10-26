2c1c970a4f398ad3a6a3a5aded6c7dd688cbc3bc
Joao Diogo Trindade Guarda <up201303463@B03AV1406.fe.up.pt>
2015-10-26 18:25:16 +0000

    starting development

---
+++ src/interface.pl
@@ -0,0 +1,29 @@
+rowIdentifiers([' 1 ',' 2 ', ' 3 ',' 4 ',' 5 ',' 6 ',' 7 ',' 8 ', ' 9 ']).
+
+getSymbol(emptyCell, ' ').
+getSymbol(white, 'O').
+getSymbol(black, '#').
+
+
+printColumnId :- write('      A     B     C     D     E     F     G     H     I'),nl.
+printInitialSeparator :- write('    _____________________________________________________ '),nl.
+printMiddleSeparator :- write('   |     |     |     |     |     |     |     |     |     |'),nl.
+printFinalSeparator :-  write('   |_____|_____|_____|_____|_____|_____|_____|_____|_____|'),nl.
+
+printCell(Char) :- write('|  '), write(Char), write('  ').
+printBoardLine([]) :- write('|'), nl, printFinalSeparator.
+printBoardLine([Head|Tail]) :- getSymbol(Head,Char), printCell(Char), printBoardLine(Tail).
+
+printBoardAux([],[]) :- nl.
+printBoardAux([Head|Tail], [RowId|RowTail]) :- printMiddleSeparator, write(RowId), printBoardLine(Head), printBoardAux(Tail, RowTail).
+
+printBoard(Board) :- printColumnId, printInitialSeparator, rowIdentifiers(RowId), printBoardAux(Board, RowId).
+
+
+getChar(Input):-
+	get_char(Input),
+	get_char(_).
+
+getInt(Input):-
+	get_code(TempInput),
+	Input is TempInput - 48.
